17d7237348d49a6159a787121c29b2940c35cd86
muhlis.cm <muhlis.cm@scrapehero.com>
2024-03-12 01:04:50 +0530

    commiting base model pipeline

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+
 from cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,6 +9,17 @@ STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f"<<<<<<<<<<<< stage {STAGE_NAME} started >>>>>>>>>>>>>>")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"<<<<<<<<<<<< stage {STAGE_NAME} completed >>>>>>>>>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logger.info(f"<<<<<<<<<<<< stage {STAGE_NAME} started >>>>>>>>>>>>>>")
+    obj = PrepareBaseModelPipeline()
     obj.main()
     logger.info(f"<<<<<<<<<<<< stage {STAGE_NAME} completed >>>>>>>>>>>>>>")
 except Exception as e:
